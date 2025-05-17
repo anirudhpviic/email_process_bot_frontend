14e13254fe987402020f35f91544ea10e7e136ae
anirudhpviic <anirudh.pv.iic.0106@example.com>
2025-05-17 15:23:57 +0530

    add: sub-sentiments and recurring

--- src/pages/analyze.jsx
+++ src/pages/analyze.jsx
@@ -47,9 +47,6 @@ export default function Analyze() {
               <div key={summaryData?.index} className="flex flex-col gap-3">
                 <CardHeader className="flex">
                   {summaryData?.index + ")"}
-                  {/* <Button onClick={() => setOpenEmailIndex(summaryData?.index)}>
-                    Show Email
-                  </Button> */}
                   <a
                     onClick={() => setOpenEmailIndex(summaryData?.index)}
                     className="ml-2 underline text-blue-500 cursor-pointer"
@@ -57,25 +54,12 @@ export default function Analyze() {
                     Show Email
                   </a>
                 </CardHeader>
-                <Card className="border-l-4 border-l-emerald-500 bg-emerald-50">
-                  <div className="p-6">
-                    <h2 className="text-xl font-medium text-slate-700 mb-2">
-                      Email summary
-                    </h2>
-                    <Separator className="bg-emerald-200 mb-4" />
-                    <div className="pl-4 border-l-2 border-emerald-500">
-                      <p className="text-slate-700">
-                        {summaryData?.emailSummary}
-                      </p>
-                    </div>
-                  </div>
-                </Card>
 
                 {/* Issues Section */}
                 <Card className="border-l-4 border-l-cyan-500 bg-cyan-50">
                   <div className="p-6">
                     <h2 className="text-xl font-medium text-slate-700 mb-2">
-                      Issues:
+                      Key Issues:
                     </h2>
                     <Separator className="bg-cyan-200 mb-4" />
                     <ul className="space-y-3">
@@ -83,24 +67,6 @@ export default function Analyze() {
                         <li key={index} className="flex items-start">
                           <span className="inline-block w-2 h-2 rounded-full bg-cyan-500 mt-2 mr-3"></span>
                           <span className="text-slate-700">{issue}</span>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </Card>
-
-                Key Insights Section
-                <Card className="border-l-4 border-l-amber-500 bg-amber-50">
-                  <div className="p-6">
-                    <h2 className="text-xl font-medium text-slate-700 mb-2">
-                      Key Insights:
-                    </h2>
-                    <Separator className="bg-amber-200 mb-4" />
-                    <ul className="space-y-3">
-                      {summaryData?.customerInsights.map((insight, index) => (
-                        <li key={index} className="flex items-start">
-                          <span className="inline-block w-2 h-2 rounded-full bg-amber-500 mt-2 mr-3"></span>
-                          <span className="text-slate-700">{insight}</span>
                         </li>
                       ))}
                     </ul>
@@ -115,28 +81,53 @@ export default function Analyze() {
                     </h2>
                     <Separator className="bg-purple-200 mb-4" />
                     <div className="pl-4 border-l-2 border-purple-500">
-                      <p className="text-slate-700">{summaryData?.sentiment}</p>
                       <p className="text-slate-700">
-                        Sentiment Score: {summaryData?.sentimentScore}%
+                        <span className="font-bold">Over all sentiment:</span>{" "}
+                        {summaryData?.sentiment}
                       </p>
-                    </div>
-                  </div>
-                </Card>
-
-                {/* Reason for Performance Section */}
-                <Card className="border-l-4 border-l-purple-500 bg-purple-50">
-                  <div className="p-6">
-                    <h2 className="text-xl font-medium text-slate-700 mb-2">
-                      Reason for Sentiment:
-                    </h2>
-                    <Separator className="bg-purple-200 mb-4" />
-                    <div className="pl-4 border-l-2 border-purple-500">
                       <p className="text-slate-700">
+                        <span className="font-bold">Sentiment Score:</span>{" "}
+                        {summaryData?.sentimentScore}%
+                      </p>
+                      <p className="text-slate-700">
+                        <span className="font-bold">Reason for Sentiment:</span>{" "}
                         {summaryData?.reasonForSentiment}
                       </p>
                     </div>
+                    {summaryData?.subSentiments?.length > 0 ? (
+                      <>
+                        <Separator className="bg-purple-200 my-4" />
+                        <div className="pl-4 border-l-2 border-purple-500">
+                          <p className="text-slate-700 font-bold">
+                            Sub sentiments:
+                          </p>
+                          {summaryData?.subSentiments?.map((sub, index) => (
+                            <p className="text-slate-700" key={index}>
+                              {sub?.sentiment}: {sub?.score}%
+                            </p>
+                          ))}
+                        </div>
+                      </>
+                    ) : null}
                   </div>
                 </Card>
+
+                {/* Recurring */}
+                {summaryData?.isRecurring ? (
+                  <Card className="border-l-4 border-l-purple-500 bg-purple-50">
+                    <div className="p-6">
+                      <h2 className="text-xl font-medium text-slate-700 mb-2">
+                        Recurring Issue:
+                      </h2>
+                      <Separator className="bg-purple-200 mb-4" />
+                      <div className="pl-4 border-l-2 border-purple-500">
+                        <p className="text-slate-700">
+                          {summaryData?.recurringExplain}
+                        </p>
+                      </div>
+                    </div>
+                  </Card>
+                ) : null}
 
                 {/* Email Dialog */}
                 <Dialog
